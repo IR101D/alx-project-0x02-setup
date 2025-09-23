a588a83e4aa6a95106f332e1c9746435523ff49a
IR101D <somefkingadress@gmail.com>
2025-09-23 22:19:51 +0100

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 import { useEffect, useState } from "react";
 import { PostProps } from "@/interfaces";
+import { GetStaticProps } from "next";
 
 const Posts  : React.FC = () => {
    const [posts, setPosts] = useState<PostProps[]>([]);
